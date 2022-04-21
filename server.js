165ec249e7bde8a707d4d708fa07a06b05bd17bc
M8MBA <sauce.ix@gmail.com>
2022-04-21 15:17:09 -0600

    tests

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ const sess = {
 
 app.use(session(sess));
 
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
 const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
